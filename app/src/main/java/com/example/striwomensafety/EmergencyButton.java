39f4833b2c680b2878aeeada6f1605963a77282f
Chandraparsad3 <chandraparsad02@gmail.com>
2024-01-11 08:34:02 +0530

    SMS emergency updated

--- app/src/main/java/com/example/striwomensafety/EmergencyButton.java
+++ app/src/main/java/com/example/striwomensafety/EmergencyButton.java
@@ -1,7 +1,9 @@
 package com.example.striwomensafety;
 
 import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -14,18 +16,21 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 public class EmergencyButton extends AppCompatActivity {
 
-    private static final int SMS_PERMISSION_REQUEST_CODE = 2; // or any other value
-
+    private static final int SMS_PERMISSION_REQUEST_CODE = 2;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
-    private ImageButton  emergencyButton;
+    private ImageButton emergencyButton;
     private ImageView sidebarIcon;
+
+    private boolean emergencyConfirmed = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,9 +43,9 @@ public class EmergencyButton extends AppCompatActivity {
             finish(); // finish this activity to prevent the user from going back
             return;
         }
-        checkSmsPermission();
-        // Check and request location permissions
-        checkLocationPermission();
+
+        // Check and request SMS and location permissions
+        checkSmsAndLocationPermissions();
 
         setContentView(R.layout.activity_emergency_button);
 
@@ -52,54 +57,60 @@ public class EmergencyButton extends AppCompatActivity {
         emergencyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                handleEmergencyButtonClick();
-                showToast("Emergency");
+                showConfirmationDialog();
             }
         });
         sidebarIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    showToast("Side bar clicked");
+                showToast("Sidebar clicked");
             }
         });
     }
 
     private boolean isSetupCompleted() {
-        // Check if setup is completed by verifying if the numbers are stored in SharedPreferences
         SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
         return sharedPreferences.contains("EmergencyContact1")
                 && sharedPreferences.contains("EmergencyContact2")
                 && sharedPreferences.contains("EmergencyContact3");
     }
 
+    private void showConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirmation");
+        builder.setMessage("Are you sure you want to send an emergency message?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                emergencyConfirmed = true;
+                handleEmergencyButtonClick();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Do nothing if the user clicks "No"
+            }
+        });
+        builder.show();
+    }
+
     private void handleEmergencyButtonClick() {
-        // Check location permissions
-        showToast("This also works");
+        if (!emergencyConfirmed) {
+            // If emergency is not confirmed, do nothing
+            return;
+        }
+
+        // Check location permission and enable location services
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            // Check if location services are enabled
             if (isLocationServicesEnabled()) {
-                // Get current location
-                showToast("Location is enabled");
-                Location currentLocation = getCurrentLocation();
-
-                // You can now use the 'currentLocation' to send location details in your emergency message
-                if (currentLocation != null) {
-                    double latitude = currentLocation.getLatitude();
-                    double longitude = currentLocation.getLongitude();
-
-                    // Create a Google Maps link
-                    String googleMapsLink = "http://maps.google.com/maps?q=" + latitude + "," + longitude;
-                    // Send emergency message
-                    sendEmergencyMessage(googleMapsLink);
-                }
+                sendEmergencyMessage();
             } else {
                 showToast("Location services are not enabled. Please enable them in settings.");
             }
         } else {
-            // Request location permissions
-            checkLocationPermission();
-            // showToast("Location permissions not granted."); // This line is now commented
+            checkSmsAndLocationPermissions();
         }
     }
 
@@ -108,114 +119,126 @@ public class EmergencyButton extends AppCompatActivity {
         return locationManager != null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
 
-    private void checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // If permission is not granted, request it
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE);
-        } else {
-            handleEmergencyButtonClick();
-        }
-    }
-    private void checkSmsPermission() {
+    private void checkSmsAndLocationPermissions() {
+        // Check SMS permission
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
                 != PackageManager.PERMISSION_GRANTED) {
-            // If permission is not granted, request it
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.SEND_SMS},
                     SMS_PERMISSION_REQUEST_CODE);
-        } else {
-            // Handle your SMS-related logic
-            handleEmergencyButtonClick();
+        }
+
+        // Check location permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
         }
     }
-
-
-
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         if (requestCode == SMS_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                handleEmergencyButtonClick();
+                // SMS permission granted, check location permission
+                checkLocationPermission();
             } else {
                 showToast("SMS permission denied. Cannot send emergency message.");
             }
+        } else if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Location permission granted, handle emergency button click
+                handleEmergencyButtonClick();
+            } else {
+                showToast("Location permission denied. Cannot get current location.");
+            }
         }
-
-        // Call super method
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            // Location permission granted, handle emergency button click
+            handleEmergencyButtonClick();
+        }
+    }
 
     private Location getCurrentLocation() {
-        // Check location permissions
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
- showToast("OKI");
-            if (locationManager != null) {
-
-                showToast("location not null ");
-                try {
-                    // Check if GPS provider is enabled
-                    if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                        showToast("Location GPS enabled");
-                        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    } else if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-                        // If GPS provider is not enabled, try using NETWORK_PROVIDER
-                        showToast("Location network enabled");
-                        return locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                    } else {
-                        // Handle the case when neither GPS nor NETWORK provider is enabled
-                        showToast("Location providers are not enabled.");
-                    }
-                } catch (SecurityException e) {
-                    showToast("ERROR");
-                    e.printStackTrace();
-
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager != null) {
+            try {
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                } else if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                    return locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                } else {
+                    showToast("Location providers are not enabled.");
                 }
+            } catch (SecurityException e) {
+                e.printStackTrace();
             }
-        } else {
-            checkLocationPermission();
         }
-        showToast("NULL return");
         return null;
     }
 
+    private void sendEmergencyMessage() {
+        Location currentLocation = getCurrentLocation();
 
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        if (currentLocation != null) {
+            double latitude = currentLocation.getLatitude();
+            double longitude = currentLocation.getLongitude();
+
+            String googleMapsLink = "http://maps.google.com/maps?q=" + latitude + "," + longitude;
+            sendEmergencyMessage(googleMapsLink);
+        } else {
+            showToast("Unable to get current location.");
+        }
     }
 
     private void sendEmergencyMessage(String locationLink) {
-        // Retrieve contact numbers from SharedPreferences
         SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
         String contactNumber1 = sharedPreferences.getString("EmergencyContact1", null);
         String contactNumber2 = sharedPreferences.getString("EmergencyContact2", null);
         String contactNumber3 = sharedPreferences.getString("EmergencyContact3", null);
 
-        // Check if valid contact numbers are available
         if (contactNumber1 != null && !contactNumber1.isEmpty() &&
                 contactNumber2 != null && !contactNumber2.isEmpty() &&
                 contactNumber3 != null && !contactNumber3.isEmpty()) {
 
-            // Use SmsManager to send the emergency message
             SmsManager smsManager = SmsManager.getDefault();
             String message = "Emergency! I need help. My location: " + locationLink;
 
-            // Send the message to all three contact numbers
-            smsManager.sendTextMessage(contactNumber1, null, message, null, null);
-            smsManager.sendTextMessage(contactNumber2, null, message, null, null);
-            smsManager.sendTextMessage(contactNumber3, null, message, null, null);
+            // Use a loop to send messages to each contact number
+            String[] contactNumbers = {contactNumber1, contactNumber2, contactNumber3};
+            for (int i = 0; i < contactNumbers.length; i++) {
+                sendSms(smsManager, contactNumbers[i], message, i);
+            }
 
-            // Display a confirmation message
             showToast("Emergency message sent to contacts.");
         } else {
             showToast("Incomplete setup. Please set three emergency contact numbers in settings.");
         }
+    }
+
+    private void sendSms(SmsManager smsManager, String contactNumber, String message, int index) {
+        Intent sentIntent = new Intent("SMS_SENT");
+        PendingIntent sentPendingIntent = PendingIntent.getBroadcast(this, index, sentIntent, PendingIntent.FLAG_IMMUTABLE);
+
+        Intent deliveredIntent = new Intent("SMS_DELIVERED");
+        PendingIntent deliveredPendingIntent = PendingIntent.getBroadcast(this, index, deliveredIntent, PendingIntent.FLAG_IMMUTABLE);
+
+        smsManager.sendTextMessage(contactNumber, null, message, sentPendingIntent, deliveredPendingIntent);
+    }
+
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
